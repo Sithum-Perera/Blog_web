af8cb15903c8dc4bfb11016271ccfee3dafc8e69
SithumPerera <sithumudara677@gmail.com>
2025-05-05 03:15:35 +0530

    navBar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import { Routes, Route } from "react-router-dom";
+import Blogs from "./pages/Blogs";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import SingleBlog from "./pages/SingleBlog";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Signup />} />
+        <Route path="/blog/:id" element={<SingleBlog />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { assets } from "../assets/assets";
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav className='bg-white p-4 sticky top-0'>
+      <div className='flex container mx-auto justify-between items-center'>
+        <div className='flex gap-2 items-center'>
+        <Link to='/'>
+          <img src={assets.logo} alt="" />
+        </Link>
+        </div>
+        <ul className='hidden sm:flex gap-5 text-xl front-normal justify-center items-center text-gray-700'>
+          <Link to='/' className='cursor-pointer hover:text-green-400'>Home</Link>
+          <Link to='/blogs' className='cursor-pointer hover:text-green-400'>BLOGS</Link>
+          <Link to='/about' className='cursor-pointer hover:text-green-400'>ABOUT</Link>
+          <Link to='/contact' className='cursor-pointer hover:text-green-400'>CONTACT</Link>
+        </ul>
+        <Link className='bg-green-400 text-white px-8 py-2 rounded-full cursor-pointer hover:bg-green-600 duration-300'>Sign In</Link>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
